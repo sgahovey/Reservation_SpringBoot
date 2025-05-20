d935c36bff398a74f8e3562eced4ac6e8d68aa21
AHOVEY Saint-George <sgahovey@gmail.com>
2025-05-20 14:04:41 +0400

    Création de Utilisateur Repository + ajout de l'authentification

---
+++ src/main/java/com/dreams/hellowordspring/reservation/Repository/UtilisateurRepository.java
@@ -0,0 +1,12 @@
+package com.dreams.hellowordspring.reservation.Repository;
+
+import com.dreams.hellowordspring.reservation.Model.Utilisateur;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UtilisateurRepository extends CrudRepository<Utilisateur, Long> {
+
+    // Authentification par pseudo et mot de passe
+    Optional<Utilisateur> findByPseudoAndPassword(String pseudo, String password);
+}
